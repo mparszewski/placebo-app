111588480738ae627a40af29c0ededf9e06a9e4a
mparszewski <33162624+mparszewski@users.noreply.github.com>
2018-06-02 12:44:33 +0200

    second commit

--- src/main/java/com/example/placebo/PlaceboApplication.java
+++ src/main/java/com/example/placebo/PlaceboApplication.java
@@ -2,9 +2,16 @@ package com.example.placebo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+@EntityScan(
+        basePackageClasses = {PlaceboApplication.class, Jsr310JpaConverters.class}
+)
 
 @SpringBootApplication
 public class PlaceboApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(PlaceboApplication.class, args);
